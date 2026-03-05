51c024b738fb5c31f6c52797cc548b82d6c7c238
Onana Embolo Emile Junior <juniorembolo99@gmail.com>
2026-03-05 18:49:34 +0100

    Test du webhook111

--- ci-cd_pipeline/src/main/java/org/example/Main.java
+++ ci-cd_pipeline/src/main/java/org/example/Main.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("Hello world");
             System.out.println("Hello world2");
+            System.out.println("Hello world3");
+            System.out.println("Hello world4");
         }
     }
 }
